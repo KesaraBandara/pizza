de218793c8e4528731c1fcd1a96677ecd61d6ad9
kesara <kesrabandara1998@gmail.com>
2024-06-05 21:19:27 +0530

    signup interface

---
+++ src/screens/SignupScreen.tsx
@@ -0,0 +1,168 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { TextInput } from 'react-native';
+import {Icon} from '@rneui/themed';
+
+
+
+function SignupSection() {
+    return (
+      <View style={{marginTop: 70}}>
+        <View
+          style={{
+            backgroundColor: 'white',
+            borderRadius: 20,
+            height: 50,
+            marginHorizontal: 30,
+            justifyContent: 'center',
+            paddingLeft: 20,
+          }}>
+          <TextInput
+            placeholder="Name"
+            placeholderTextColor={'black'}
+            style={{fontSize: 15}}
+          />
+        </View>
+
+        <View
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{
+            backgroundColor: 'white',
+            borderRadius: 20,
+            height: 50,
+            marginHorizontal: 30,
+            justifyContent: 'center',
+            paddingLeft: 20,
+            marginTop: 10,
+          }}>
+          <TextInput
+            placeholder="Your Email"
+            placeholderTextColor={'black'}
+            style={{fontSize: 15}}
+          />
+        </View>
+
+        <View
+          style={{
+            backgroundColor: 'white',
+            borderRadius: 20,
+            height: 50,
+            marginHorizontal: 30,
+            justifyContent: 'center',
+            paddingLeft: 20,
+            marginTop: 10,
+          }}>
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor={'black'}
+            style={{fontSize: 15}}
+          />
+        </View>
+        
+        <SingunButton />
+        <BottomeSection />
+      </View>
+    );
+  }
+
+  function SingunButton() {
+    return (
+      <View style={{flexDirection: 'row', marginTop: 10}}>
+        <View style={{height: 70, flex: 1}}>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 20,
+              fontWeight: 700,
+              marginLeft: 40,
+            }}>
+            Sign Up
+          </Text>
+        </View>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'flex-end',
+            height: 70,
+            flex: 1,
+          }}>
+          <View
+            style={{
+              height: 50,
+              width: 50,
+              backgroundColor: '#367cfe',
+              marginRight: 40,
+              borderRadius: 200,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Icon
+              size={20}
+              color={'white'}
+              name={'arrow-forward'}
+              type="ionicon"
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+  function BottomeSection() {
+    return (
+      <View style={{flexDirection: 'row', marginTop: 10}}>
+        <View style={{height: 70, flex: 1, alignItems: 'flex-end',justifyContent:'center'}}>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 15,
+              fontWeight: 700,
+              marginRight: 40,
+            }}>
+            Sign in 
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
+
+
+const SignupScreen = () => {
+  return (
+    <View style={sty.container}>
+    <Image
+      style={{width: '100%', height: '100%', position: 'absolute'}}
+      source={require('../../assets/img/img.jpg')}
+    />
+    <Text
+      style={{
+        fontSize: 45,
+        color: 'white',
+        fontWeight: 700,
+        marginTop: 100,
+        marginLeft: 20,
+      }}>
+      {'Create \nAccount'}
+    </Text>
+    <KeyboardAwareScrollView keyboardShouldPersistTaps="never">
+      <SignupSection />
+    </KeyboardAwareScrollView>
+  </View>
+  )
+}
+
+
+const sty = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#yellow',
+    },
+  });
+
+
+export default SignupScreen
+
