673a3329a511a83bb7d040729903ce5ec52410fd
kesara <kesrabandara1998@gmail.com>
2024-06-06 15:56:27 +0530

    add navigation to login and signup

--- App.tsx
+++ App.tsx
@@ -6,113 +6,21 @@
  * @format
  */
 
+// import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import {Icon} from '@rneui/themed';
-
-function LoginField() {
-  return (
-    <View style={{marginTop: 100}}>
-      <View
-        style={{
-          backgroundColor: 'white',
-          borderRadius: 20,
-          height: 50,
-          marginHorizontal: 30,
-          justifyContent: 'center',
-          paddingLeft: 20,
-        }}>
-        <TextInput
-          placeholder="Your Email"
-          placeholderTextColor={'black'}
-          style={{fontSize: 15}}
-        />
-      </View>
-
-      <View
-        style={{
-          backgroundColor: 'white',
-          borderRadius: 20,
-          height: 50,
-          marginHorizontal: 30,
-          justifyContent: 'center',
-          paddingLeft: 20,
-          marginTop: 10,
-        }}>
-        <TextInput
-          placeholder="Password"
-          placeholderTextColor={'black'}
-          style={{fontSize: 15}}
-        />
-      </View>
-      <SinginButton />
-    </View>
-  );
-}
-
-function SinginButton() {
-  return (
-    <View style={{flexDirection: 'row', marginTop: 10}}>
-      <View style={{height: 70, flex: 1}}>
-        <Text
-          style={{
-            color: 'black',
-            fontSize: 20,
-            fontWeight: 700,
-            marginLeft: 40,
-          }}>
-          Sign In
-        </Text>
-      </View>
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'flex-end',
-          height: 70,
-          flex: 1,
-        }}>
-        <View
-          style={{
-            height: 50,
-            width: 50,
-            backgroundColor: '#367cfe',
-            marginRight: 40,
-            borderRadius: 200,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Icon
-            size={20}
-            color={'white'}
-            name={'arrow-forward'}
-            type="ionicon"
-          />
-        </View>
-      </View>
-    </View>
-  );
-}
+import {StyleSheet, View} from 'react-native';
+import AppNavigation from './src/navigations/AppNavigation';
+import 'react-native-gesture-handler';
+// import SignupScreen from './src/screens/SignupScreen.tsx';
+// import LoginScreen from './src/screens/LoginScreen.tsx';
 function App(): React.JSX.Element {
   return (
     <View style={sty.container}>
-      <Image
-        style={{width: '100%', height: '100%', position: 'absolute'}}
-        source={require('./assets/img/img.jpg')}
-      />
-      <Text
-        style={{
-          fontSize: 45,
-          color: 'white',
-          fontWeight: 700,
-          marginTop: 100,
-          marginLeft: 20,
-        }}>
-        {'Welcome \nBack'}
-      </Text>
-      <KeyboardAwareScrollView keyboardShouldPersistTaps="never">
-        <LoginField />
-      </KeyboardAwareScrollView>
+
+      <AppNavigation/>
+      {/* <NavigationContainer> </NavigationContainer> */}
+      {/* <LoginScreen/> */}
+      {/* <SignupScreen/> */}
     </View>
   );
 }
