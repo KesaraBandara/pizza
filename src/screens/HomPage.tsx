bb6cc127fcd6eb2c230bcb431a86ec6123989717
kesara <kesrabandara1998@gmail.com>
2024-06-07 01:55:29 +0530

    add navigation to home page

---
+++ src/screens/HomPage.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const HomPage = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 30, color: 'black'}}>HomPage</Text>
+    </View>
+  );
+};
+
+export default HomPage;
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable prettier/prettier */
 import React from 'react';
@@ -13,94 +15,15 @@ import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {TextInput} from 'react-native';
 import {Icon} from '@rneui/themed';
 
-function LoginField() {
-  return (
-    <View style={{marginTop: 100}}>
-      <View
-        style={{
-          backgroundColor: 'white',
-          borderRadius: 20,
-          height: 50,
-          marginHorizontal: 30,
-          justifyContent: 'center',
-          paddingLeft: 20,
-        }}>
-        <TextInput
-          placeholder="Your Email"
-          placeholderTextColor={'black'}
-          style={{fontSize: 15}}
-        />
-      </View>
 
-      <View
-        // eslint-disable-next-line react-native/no-inline-styles
-        style={{
-          backgroundColor: 'white',
-          borderRadius: 20,
-          height: 50,
-          marginHorizontal: 30,
-          justifyContent: 'center',
-          paddingLeft: 20,
-          marginTop: 10,
-        }}>
-        <TextInput
-          placeholder="Password"
-          placeholderTextColor={'black'}
-          style={{fontSize: 15}}
-        />
-      </View>
-      <SinginButton />
-      <BottomeSection />
-    </View>
-  );
-}
+function BottomeSection(p:any) {
 
-function SinginButton() {
-  return (
-    <View style={{flexDirection: 'row', marginTop: 10}}>
-      <View style={{height: 70, flex: 1}}>
-        <Text
-          style={{
-            color: 'black',
-            fontSize: 20,
-            fontWeight: 700,
-            marginLeft: 40,
-          }}>
-          Sign In
-        </Text>
-      </View>
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'flex-end',
-          height: 70,
-          flex: 1,
-        }}>
-        <View
-          style={{
-            height: 50,
-            width: 50,
-            backgroundColor: '#367cfe',
-            marginRight: 40,
-            borderRadius: 200,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Icon
-            size={20}
-            color={'white'}
-            name={'arrow-forward'}
-            type="ionicon"
-          />
-        </View>
-      </View>
-    </View>
-  );
-}
-function BottomeSection() {
+  const stack = p.bs_stack;
 
-  function gotoSignup() {}
-
+  function gotoSignup() {
+    stack.navigate('Signup');
+  }
+      
   return (
     <View style={{flexDirection: 'row', marginTop: 10}}>
       <TouchableOpacity onPress={gotoSignup}>
@@ -144,35 +67,137 @@ function BottomeSection() {
   );
 }
 
-const LoginScreen = () => {
+
+function SinginButton(p:any) {
+
+function gotoHomPage() {
+  p.sb_stack.navigate('HomPage');
+}
+
   return (
-    <View style={sty.container}>
-      <Image
-        style={{width: '100%', height: '100%', position: 'absolute'}}
-        source={require('../../assets/img/img.jpg')}
-      />
-      <Text
+    <View style={{flexDirection: 'row', marginTop: 10}}>
+      <View style={{height: 70, flex: 1}}>
+        <Text
+          style={{
+            color: 'black',
+            fontSize: 20,
+            fontWeight: 700,
+            marginLeft: 40,
+          }}>
+          Sign In
+        </Text>
+      </View>
+    <TouchableOpacity activeOpacity={0.5} onPress={gotoHomPage}>
+    <View
         style={{
-          fontSize: 45,
-          color: 'white',
-          fontWeight: 700,
-          marginTop: 100,
-          marginLeft: 20,
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+          height: 70,
+          flex: 1,
         }}>
-        {'Welcome \nBack'}
-      </Text>
-      <KeyboardAwareScrollView keyboardShouldPersistTaps="never">
-        <LoginField />
-      </KeyboardAwareScrollView>
+        <View
+          style={{
+            height: 50,
+            width: 50,
+            backgroundColor: '#367cfe',
+            marginRight: 40,
+            borderRadius: 200,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Icon
+            size={20}
+            color={'white'}
+            name={'arrow-forward'}
+            type="ionicon"
+          />
+        </View>
+      </View>
+      </TouchableOpacity>
     </View>
   );
-};
+}
 
-const sty = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#yellow',
-  },
-});
+function LoginField(p:any) {
+
+  const stack = p.lf_stack;
+  return (
+    <View style={{marginTop: 100}}>
+      <View
+        style={{
+          backgroundColor: 'white',
+          borderRadius: 20,
+          height: 50,
+          marginHorizontal: 30,
+          justifyContent: 'center',
+          paddingLeft: 20,
+        }}>
+        <TextInput
+          placeholder="Your Email"
+          placeholderTextColor={'black'}
+          style={{fontSize: 15}}
+        />
+      </View>
+
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          backgroundColor: 'white',
+          borderRadius: 20,
+          height: 50,
+          marginHorizontal: 30,
+          justifyContent: 'center',
+          paddingLeft: 20,
+          marginTop: 10,
+        }}>
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor={'black'}
+          style={{fontSize: 15}}
+        />
+      </View>
+      <SinginButton sb_stack={stack} />
+      <BottomeSection bs_stack={stack}/>
+    </View>
+  );
+}
+
+
+const LoginScreen = (ls_props: any) => {
+
+  const stack = ls_props.navigation;
+  
+    return (
+      <View style={sty.container}>
+        <Image
+          style={{width: '100%', height: '100%', position: 'absolute'}}
+          source={require('../../assets/img/img.jpg')}
+        />
+        <Text
+          style={{
+            fontSize: 45,
+            color: 'white',
+            fontWeight: 700,
+            marginTop: 100,
+            marginLeft: 20,
+          }}>
+          {'Welcome \nBack'}
+        </Text>
+        <KeyboardAwareScrollView keyboardShouldPersistTaps="never">
+          <LoginField lf_stack={stack} />
+        </KeyboardAwareScrollView>
+      </View>
+    );
+  };
+  
+  const sty = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#yellow',
+    },
+  });
+
+
+
 
 export default LoginScreen;
